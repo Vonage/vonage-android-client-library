8fee556aab080b3dfa66aff85a1a7e71e018b73e
Chris Tankersley <chris@ctankersley.com>
2026-07-05 19:58:21 -0400

    Bump version to 1.3.0-alpha02

--- client-library/build.gradle.kts
+++ client-library/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
 
-val libraryVersion = "1.3.0-alpha01"
+val libraryVersion = "1.3.0-alpha02"
 
 plugins {
     id("com.android.library")
